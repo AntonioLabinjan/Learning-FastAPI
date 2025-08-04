90ef5bfea8c4c45ce95a7189c4d9245b77d24890
Antonio Labinjan <alabinjan6@gmail.com>
2025-08-04 19:06:21 +0200

    importovi su zajebavali

--- crud.py
+++ crud.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Session
-from . import models, schemas
+from app import schemas, models
 
 def get_item(db: Session, item_id: int):
     return db.query(models.Item).filter(models.Item.id == item_id).first()
